6a01635edb20f65bd3f270e10eceb0c99faf1059
Zeph Nord <zeph.nord@gmail.com>
2016-07-26 13:03:42 -0500

    Simplifying fractions submission

---
+++ 2016-Challenges/07-26-2016/DailyProgrammer_07_26_16.cs
@@ -0,0 +1,56 @@
+﻿namespace DailyProgrammingChallenges
+{
+    class DailyProgrammer_072616
+    {
+        public static void ReduceFraction(ref int numerator, ref int denominator)
+        {
+            
+            if (numerator >= denominator)
+            {
+                int divisor = denominator;
+                while (denominator > 1 && numerator > 1 && divisor > 1)
+                {
+                    if (numerator%divisor == 0 && denominator%divisor == 0)
+                    {
+                        denominator = denominator/divisor;
+                        numerator = numerator/divisor;
+                        divisor = numerator;
+                    }
+                    else
+                    {
+                        divisor--;
+                    }
+                }
+            }
+            else
+            {
+                int divisor = numerator;
+                while (denominator > 1 && numerator > 1 && divisor > 1)
+                {
+                    if (denominator%divisor == 0 && numerator%divisor == 0)
+                    {
+                        denominator = denominator/divisor;
+                        numerator = numerator/divisor;
+                        divisor = numerator;
+                    }
+                    else
+                    {
+                        divisor--;
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i = i + 2)
+            {
+                var numerator = int.Parse(args[i]);
+                var denominator = int.Parse(args[i + 1]);
+
+                ReduceFraction(ref numerator, ref denominator);
+                System.Console.WriteLine(numerator + " " + denominator + "\n");
+            }
+        }
+    }
+}
